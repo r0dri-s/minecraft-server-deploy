d4fd74ebf78e09f3905ce75769dee73cb7e5782b
r0dri-s <108372469+r0dri-s@users.noreply.github.com>
2023-03-10 16:33:15 +0000

    (Almost) complete rewrite. Added functions and fixed the java 8 bug

--- deploy_server.sh
+++ deploy_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-\\----------------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------------
 Normal(){
 
 mkdir "$name"
@@ -24,19 +24,41 @@ echo "eula.txt copied!"
 wget "https://raw.githubusercontent.com/r0dri-s/minecraft-server-deploy/main/other_stuff/server-icon.png" -O "$name"/server-icon.png -q
 echo "server-icon.png copied!"
 sed -i "s/^motd=.*/motd="$name"/" "$name"/server.properties
-sed -i "s/^java -Xmx8192M -jar server.jar -nogui/java -Xmx"$ram"M -jar server.jar -nogui/" "$name"/run.sh
+case $sn in
+        yes )sed -i "s/^java -Xmx8192M -jar server.jar -nogui/usr/java-8/jre1.8.0_361/bin/java -Xmx"$ram"M -jar server.jar -nogui/" "$name"/run.sh
+        no ) sed -i "s/^java -Xmx8192M -jar server.jar -nogui/java -Xmx"$ram"M -jar server.jar -nogui/"
+ "$name"/run.sh
 echo "Server deployed!"
 }
-\\---------------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------------
 Default(){
 read -p "enter your server's name: " -e name
 read -p "enter your server's RAM (mb): " -e ram
 read -p "enter your server's version [1.19.3]: " -e version
 version=${version:-1.19.3}
-Normal
-}
-\\---------------------------------------------------------------------------------------------------------------------------------------
 
+if (( $version < 1.17.1 )); then
+  BadJava
+else
+  Normal
+fi
+}
+#---------------------------------------------------------------------------------------------------------------------------------------
+BadJava(){
+
+read -p "The version you chose uses java 8 instead of java 17. Do you want to install it?(root required)" -e sn
+case $sn in
+        yes ) sudo mkdir /usr/java-8
+        wget "https://raw.githubusercontent.com/r0dri-s/minecraft-server-deploy/main/other_stuff/jre-8u361-linux-x64.tar.gz" -o /usr/java-8/1.tar.gz
+        tar zxvf jre-8u361-linux-x64.tar.gz
+        no ) echo ok, we will proceed without...;
+                Normal
+                exit;;
+        * ) echo invalid response;
+                exit 1;;
+
+}
+#---------------------------------------------------------------------------------------------------------------------------------------
 Custom(){
 
 read -p "enter your server's name: " -e name
@@ -45,7 +67,7 @@ read -p "enter your server's version [1.19.3]: " -e version
 version=${version:-1.19.3}
 
 }
-\\---------------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------------
 PS3='Please select your type of installation: '
 options=("Default" "Custom" "(Ultra)Custom" "Quit")
 select opt in "${options[@]}"
@@ -56,7 +78,7 @@ do
             ;;
         "Custom")
             Custom
-            ;;
+	    ;;
         "(Ultra)Custom")
             UltraCustom
             ;;
