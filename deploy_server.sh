10593fac5369bb0c25a4d33f96bf1920f2085804
r0dri-s <108372469+r0dri-s@users.noreply.github.com>
2023-03-25 09:22:23 +0000

    Fixed a TON of errors. Fully working now

--- deploy_server.sh
+++ deploy_server.sh
@@ -25,9 +25,9 @@ wget "https://raw.githubusercontent.com/r0dri-s/minecraft-server-deploy/main/oth
 echo "server-icon.png copied!"
 sed -i "s/^motd=.*/motd="$name"/" "$name"/server.properties
 case $sn in
-        yes )sed -i "s/^java -Xmx8192M -jar server.jar -nogui/usr/java-8/jre1.8.0_361/bin/java -Xmx"$ram"M -jar server.jar -nogui/" "$name"/run.sh
-        no ) sed -i "s/^java -Xmx8192M -jar server.jar -nogui/java -Xmx"$ram"M -jar server.jar -nogui/"
- "$name"/run.sh
+        yes ) sed -i "s/^java -Xmx8192M -jar server.jar -nogui/usr/java-8/jre1.8.0_361/bin/java -Xmx"$ram"M -jar server.jar -nogui/" "$name"/run.sh;;
+	    no ) sed -i "s/^java -Xmx8192M -jar server.jar -nogui/java -Xmx"$ram"M -jar server.jar -nogui" "$name"/run.sh;
+esac
 echo "Server deployed!"
 }
 #---------------------------------------------------------------------------------------------------------------------------------------
@@ -37,7 +37,7 @@ read -p "enter your server's RAM (mb): " -e ram
 read -p "enter your server's version [1.19.3]: " -e version
 version=${version:-1.19.3}
 
-if (( $version < 1.17.1 )); then
+if [[ $version < "1.17.1" ]]; then
   BadJava
 else
   Normal
@@ -46,17 +46,15 @@ fi
 #---------------------------------------------------------------------------------------------------------------------------------------
 BadJava(){
 
-read -p "The version you chose uses java 8 instead of java 17. Do you want to install it?(root required)" -e sn
-case $sn in
-        yes ) sudo mkdir /usr/java-8
-        wget "https://raw.githubusercontent.com/r0dri-s/minecraft-server-deploy/main/other_stuff/jre-8u361-linux-x64.tar.gz" -o /usr/java-8/1.tar.gz
-        tar zxvf jre-8u361-linux-x64.tar.gz
-        no ) echo ok, we will proceed without...;
-                Normal
-                exit;;
-        * ) echo invalid response;
-                exit 1;;
-
+read -p "The version you chose uses java 8 instead of java 17. Do you want to install it?(root required)" sn
+    case $sn in
+        [Yy]* ) sudo mkdir /usr/java-8
+        wget "https://raw.githubusercontent.com/r0dri-s/minecraft-server-deploy/main/other_stuff/jre-8u361-linux-x64.tar.gz" -O "/usr/java-8/1.tar.gz"
+	tar zxvf jre-8u361-linux-x64.tar.gz;;
+        [Nn]* ) echo "ok, we will proceed without..."
+                Normal;;
+        * ) echo "Please answer yes or no.";;
+    esac
 }
 #---------------------------------------------------------------------------------------------------------------------------------------
 Custom(){
@@ -69,7 +67,7 @@ version=${version:-1.19.3}
 }
 #---------------------------------------------------------------------------------------------------------------------------------------
 PS3='Please select your type of installation: '
-options=("Default" "Custom" "(Ultra)Custom" "Quit")
+options=("Default" "Custom" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -78,9 +76,6 @@ do
             ;;
         "Custom")
             Custom
-	    ;;
-        "(Ultra)Custom")
-            UltraCustom
             ;;
         "Quit")
             break
